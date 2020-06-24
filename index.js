b105825d493a3fd31130f078bbb8e079bcc8d313
chenjing <577571623@qq.com>
2020-06-24 11:48:22 +0800

    提交了index.js文件

---
+++ index.js
@@ -0,0 +1,25 @@
+//设置全局用户名,邮箱配置
+// git config --global user.name 
+//git config --global user.eamil
+
+
+//查看全部全局配置
+//git config --list --global 
+
+//获取config帮助信息
+//git config -h
+
+
+//获取git仓库的两种方式：
+//将本地文件转化成仓库  git init
+//克隆(clone)
+
+
+//查看当前文件状态
+// git status
+//精简方式显示当前文件状态
+// git status -s
+
+
+//添加追踪文件
+//git add index.html(文件名)
